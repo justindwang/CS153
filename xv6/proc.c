ff7e0bafe5d1a6a7a351390c1040e80c65e7a2ed
justindwang <justindwang@yahoo.com>
2022-02-20 18:02:22 -0800

    debugging

--- xv6/proc.c
+++ xv6/proc.c
@@ -251,8 +251,7 @@ exit(int status)
   end_op();
   curproc->cwd = 0;
 
-  int end = ticks;
-  cprintf("\nProcess %d's turnaround time was %d\n", curproc->pid, end - curproc->start);
+  cprintf("\nProcess %d's turnaround time was %d\n", curproc->pid, ticks - curproc->start);
 
   acquire(&ptable.lock);
 
