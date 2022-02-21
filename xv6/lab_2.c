fcf036a141b251e9978a8502f9ef7fd6e61671d6
justindwang <justindwang@yahoo.com>
2022-02-20 22:10:01 -0800

    debug

--- xv6/lab_2.c
+++ xv6/lab_2.c
@@ -1,56 +1,101 @@
 #include "types.h"
 #include "user.h"
 
+int priorityTest(void);
+
 int main(int argc, char *argv[])
 {
-	
-	int PScheduler(void);
-
-  printf(1, "\n This program tests the correctness of your lab#2\n");
+  printf(1, "\n This program tests the correctness of the new updatepriority systemcall and scheduling\n");
   
-	PScheduler();
+	priorityTest();
+	exit(0);
 	return 0;
  }
   
-    
-     int PScheduler(void){
-		 
-    // use this part to test the priority scheduler. Assuming that the priorities range between range between 0 to 31
-    // 0 is the highest priority and 31 is the lowest priority.  
+int priorityTest(void) {
+	int pid, exit_status, i;
+	int ret_pid = 1;
+	printf(1, "\n  Lab 2 testing priority scheduling:\n");
 
-  int pid;
-  int i,j,k;
-  
-    printf(1, "\n  Step 2: testing the priority scheduler and setpriority(int priority)) systema call:\n");
-    printf(1, "\n  Step 2: Assuming that the priorities range between range between 0 to 31\n");
-    printf(1, "\n  Step 2: 0 is the highest priority. All processes have a default priority of 10\n");
-    printf(1, "\n  Step 2: The parent processes will switch to priority 0\n");
-    setpriority(0);
-    for (i = 0; i <  3; i++) {
 	pid = fork();
-	if (pid > 0 ) {
-		continue;}
-	else if ( pid == 0) {
-
-		setpriority(30-10*i);	
-		for (j=0;j<50000;j++) {
-			for(k=0;k<1000;k++) {
-				asm("nop"); }}
-		printf(1, "\n child# %d with priority %d has finished! \n",getpid(),30-10*i);		
-		exit(0);
-        }
-        else {
-			printf(2," \n Error \n");
-			
-        }
-	}
-
-	if(pid > 0) {
-		for (i = 0; i <  3; i++) {
-			wait(0);
-
+	for (i = 0; i < 3; i++) {
+    		pid = fork();
+    		if (pid == 0) {
+			if (getpid() == 6) {
+      				printf(1, "\nThis is process PID# %d and I have priority of 1\n", getpid());
+				updatepriority(1, getpid());
+			}
+			int j;
+			for (j = 0; j < 25000000; j++) {}
+			exit(0);
 		}
-                     printf(1,"\n if processes with highest priority finished first then its correct \n");
+	}
+	if (pid == 0) {
+		int j;
+		for (j = 0; j < 25000000; j++) {}
+      		exit(0);
+      	}
+	
+	while (ret_pid > 0) {
+			ret_pid = wait(&exit_status);
+                        printf(1, "\nThis is process PID# %d and I finished.\n", ret_pid);
+	}
+	exit(0);
+	return 0;
 }
-	exit(0);		
-	return 0;}
+// #include "types.h"
+// #include "user.h"
+
+// int main(int argc, char *argv[])
+// {
+	
+// 	int PScheduler(void);
+
+//   printf(1, "\n This program tests the correctness of your lab#2\n");
+  
+// 	PScheduler();
+// 	return 0;
+//  }
+  
+    
+//      int PScheduler(void){
+		 
+//     // use this part to test the priority scheduler. Assuming that the priorities range between range between 0 to 31
+//     // 0 is the highest priority and 31 is the lowest priority.  
+
+//   int pid;
+//   int i,j,k;
+  
+//     printf(1, "\n  Step 2: testing the priority scheduler and setpriority(int priority)) systema call:\n");
+//     printf(1, "\n  Step 2: Assuming that the priorities range between range between 0 to 31\n");
+//     printf(1, "\n  Step 2: 0 is the highest priority. All processes have a default priority of 10\n");
+//     printf(1, "\n  Step 2: The parent processes will switch to priority 0\n");
+//     setpriority(0);
+//     for (i = 0; i <  3; i++) {
+// 	pid = fork();
+// 	if (pid > 0 ) {
+// 		continue;}
+// 	else if ( pid == 0) {
+
+// 		setpriority(30-10*i);	
+// 		for (j=0;j<50000;j++) {
+// 			for(k=0;k<1000;k++) {
+// 				asm("nop"); }}
+// 		printf(1, "\n child# %d with priority %d has finished! \n",getpid(),30-10*i);		
+// 		exit(0);
+//         }
+//         else {
+// 			printf(2," \n Error \n");
+			
+//         }
+// 	}
+
+// 	if(pid > 0) {
+// 		for (i = 0; i <  3; i++) {
+// 			wait(0);
+
+// 		}
+//                      printf(1,"\n if processes with highest priority finished first then its correct \n");
+// }
+// 	exit(0);		
+// 	return 0;}
